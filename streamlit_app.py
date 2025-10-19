8d7eee21318a1252487f3c1d02a692564d638f7d
BardawalBihari <bardawal5555@gmail.com>
2025-10-19 16:32:09 +0530

    Update streamlit_app.py
    remove extra line

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col, when_matched
-streamlit.title('My Parents New Healthy Diner')
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
