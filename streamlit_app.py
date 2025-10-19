cb6114d2e3021f57a8e9ebb92d8db613574b2017
BardawalBihari <bardawal5555@gmail.com>
2025-10-19 17:09:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,11 @@ session = cnx.session()
 
 # Load fruit options from Snowflake
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
-st.dataframe(data = my_dataframe, use_container_width = True)
+#st.dataframe(data = my_dataframe, use_container_width = True)
+#st.stop()
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 # Multiselect with max_selections
