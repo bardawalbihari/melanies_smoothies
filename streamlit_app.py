106f52a0715dfa140845cedc6318f3ff6effb69e
BardawalBihari <bardawal5555@gmail.com>
2025-10-19 17:16:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,19 +13,15 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-# Load fruit options from Snowflake
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
-#st.dataframe(data = my_dataframe, use_container_width = True)
-#st.stop()
-
+# Load fruit options (with SEARCH_ON) from Snowflake
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"), col("SEARCH_ON"))
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+fruit_display_names = list(pd_df['FRUIT_NAME'])
 
-# Multiselect with max_selections
+# Multiselect with max_selections=5
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
-    options=fruit_list,
+    options=fruit_display_names,
     max_selections=5
 )
 
@@ -33,10 +29,12 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        # Look up SEARCH_ON term for API
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        # Display nutrition header for each fruit
         st.subheader(f"{fruit_chosen} Nutrition Information")
-        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")
+        # Call API using SEARCH_ON value
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # Insert statement
@@ -57,3 +55,8 @@ import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 
 sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
+
+
+
+
+
